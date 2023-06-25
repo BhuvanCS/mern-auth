56dc08e33671ba95bbcee8970e20631980a4f141
Bhuvan C S <205bhuvics@gmail.com>
2023-06-25 16:51:08 +0530

    Complete Update user profile endpoint

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -78,7 +78,23 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // route PATCH /api/users/profile
 // @access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({message: "Update User Profile"});
+
+   const user = await User.findById(req.user._id);
+
+   if(user){
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    req.body.password && (user.password = req.body.password);
+    await user.save();
+    res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email
+    });
+   }else{
+    res.status(404);
+    throw new Error("User not found");
+   }
 });
 export {
     authUser,
