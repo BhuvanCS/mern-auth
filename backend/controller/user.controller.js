a77e691513ea8f798896918686e296853c484af9
Bhuvan C S <205bhuvics@gmail.com>
2023-06-25 08:37:01 +0530

    MongoDB Connection and User Register route

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,6 +1,6 @@
 import asyncHandler from 'express-async-handler';
 //Async handler to custom handle the error
-
+import User from '../models/user.model.js';
 // @desc Auth User/Set Token
 // route POST /api/users/auth
 // @access Public
@@ -12,6 +12,28 @@ const authUser = asyncHandler(async (req, res) => {
 // route POST /api/users
 // @access Public
 const regUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body;
+    const userExists = await User.findOne({email: email});
+    if(userExists){
+        res.status(400);
+        throw new Error(`${email} already registred as a user`);
+    }
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    }
+    else{
+        res.status(400);
+        throw new Error('Invalid username');
+    }
     res.status(200).json({message: "Register User"});
 });
 
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import { notFound, errorHandler } from './middleware/error.middleware.js';
+import connectDB from './config/db.js';
 import userRoutes from "./routes/user.routes.js"
+
 
 const port = process.env.PORT || 5000;
 
+connectDB();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
