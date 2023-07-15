83f2b9deb61d5b91bfb7b1c46a8a65a274211d5a
Bhuvan C S <205bhuvics@gmail.com>
2023-07-15 13:44:07 +0530

    Add Login Functionality

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -2,7 +2,15 @@ import { apiSlice } from "./apiSlice";
 const USERS_URL = '/api/users';
 
 export const usersApiSlice = apiSlice.injectEndpoints({
-    endpoint: (builder) => ({
-        login: builder.mutation()
+    endpoints: (builder) => ({
+        login: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_URL}/auth`,
+                method: 'POST',
+                body: data
+            })
+        })
     })
 })
+
+export const {useLoginMutation} = usersApiSlice;
